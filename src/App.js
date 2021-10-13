d2d41d654ac430be88d2e1791857fe3f194846cd
{eltr.kc} <{trandinhhiep789@gmail.com}>
2021-10-13 10:03:29 +0700

    feat: form with formik and yub

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import "./App.css";
-import React, { useState } from "react";
+import React from "react";
 import Snackbar from "@mui/material/Snackbar";
 import MuiAlert from "@mui/material/Alert";
 
 // material-ui
-import TextField from "@mui/material/TextField";
 import Checkbox from "@mui/material/Checkbox";
 import Grid from "@mui/material/Grid";
 import Button from "@mui/material/Button";
 
 // style-component
 import styled from "styled-components";
+import { Formik, Form, FastField } from "formik";
+import * as Yup from "yup";
+import InputField from "./custom-fields/inputField";
 
 const Background = styled.div`
   background: #e3f2fd;
@@ -34,70 +36,9 @@ const Btn = styled.div`
 `;
 
 function App() {
-  const [timer, setTimer] = useState(null);
-
   // alert
   const [openSuccess, setOpenSuccess] = React.useState(false);
   const [openError, setOpenError] = React.useState(false);
-  const [validateNumber, setValidateNumber] = useState(true);
-
-  // text-field
-  const [id, setId] = useState(null);
-  const [name, setName] = useState(null);
-  const [description, setDescription] = useState(null);
-  const [number, setNumber] = useState(null);
-
-  const handleChange = (setData, data) => {
-    if (timer) {
-      clearTimeout(timer);
-      setTimer(null);
-    }
-    setTimer(
-      setTimeout(() => {
-        setData(data);
-      }, 300)
-    );
-  };
-
-  const validateNumberField = (idNumber) => {
-    const numberRegEx = /^[0-9]+$/;
-    return numberRegEx.test(String(idNumber).toLowerCase());
-  };
-
-  const onChangeIdNumber = (setData, data) => {
-    if (timer) {
-      clearTimeout(timer);
-      setTimer(null);
-    }
-    setTimer(
-      setTimeout(() => {
-        setData(data);
-        if (data !== "") {
-          const isValid = !data || validateNumberField(data);
-          setValidateNumber(isValid);
-        }
-      }, 300)
-    );
-  };
-
-  const handleSubmit = () => {
-    if (
-      id === null ||
-      id === "" ||
-      name === null ||
-      name === "" ||
-      description === null ||
-      description === "" ||
-      number === null ||
-      number === "" ||
-      validateNumber === false
-    ) {
-      setOpenError(true);
-    } else {
-      setOpenSuccess(true);
-      console.log(id, name, description, number);
-    }
-  };
 
   const Alert = React.forwardRef(function Alert(props, ref) {
     return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
@@ -111,119 +52,108 @@ function App() {
     }
   };
 
+  const initialValues = {
+    id: "",
+    name: "",
+    description: "",
+    number: "",
+  };
+
+  const validationSchema = Yup.object().shape({
+    id: Yup.string().required("Vui lòng không bỏ trống"),
+
+    name: Yup.string().required("Vui lòng không bỏ trống"),
+
+    description: Yup.string().required("Vui lòng không bỏ trống"),
+
+    number: Yup.number()
+      .typeError("Vui lòng nhập chữ số")
+      .required("Vui lòng không bỏ trống"),
+  });
+
   return (
     <Background>
       <DivForm>
-        <Grid container spacing={2}>
-          {/* Mã loại giao dịch với đối tác */}
-          <Grid item xs={12} sm={3}>
-            <label>Mã loại giao dịch với đối tác</label>
-          </Grid>
-          <Grid item xs={12} sm={9}>
-            <TextField
-              onChange={(e) => handleChange(setId, e.target.value)}
-              size="small"
-              className="text-field"
-              autoComplete="off"
-              id="outlined-basic"
-              label="Mã loại giao dịch với đối tác"
-              variant="outlined"
-              error={Boolean(id === "" ? true : false)}
-              helperText={id === "" ? "Vui lòng không để trống" : ""}
-            />
-          </Grid>
+        <Formik
+          initialValues={initialValues}
+          validationSchema={validationSchema}
+          onSubmit={(values, { resetForm }) => {
+            console.log(values);
+            setOpenSuccess(true);
+            resetForm();
+          }}
+        >
+          {(formikProps) => {
+            // const { values, errors, touched } = formikProps;
+            return (
+              <>
+                <Form>
+                  <Grid container spacing={2}>
+                    {/* Mã loại giao dịch với đối tác */}
+                    <FastField
+                      name="id"
+                      component={InputField}
+                      label="Mã loại giao dịch với đối tác"
+                      placeholder="Mã loại giao dịch với đối tác"
+                    />
 
-          {/* Tên loại giao dịch với đối tác */}
-          <Grid item xs={12} sm={3}>
-            <label>Tên loại giao dịch với đối tác</label>
-          </Grid>
-          <Grid item xs={12} sm={9}>
-            <TextField
-              onChange={(e) => handleChange(setName, e.target.value)}
-              size="small"
-              className="text-field"
-              autoComplete="off"
-              id="outlined-basic"
-              label="Tên loại giao dịch với đối tác"
-              variant="outlined"
-              error={Boolean(name === "" ? true : false)}
-              helperText={name === "" ? "Vui lòng không để trống" : ""}
-            />
-          </Grid>
+                    {/* Tên loại giao dịch với đối tác */}
+                    <FastField
+                      name="name"
+                      component={InputField}
+                      label="Tên loại giao dịch với đối tác"
+                      placeholder="Tên loại giao dịch với đối tác"
+                    />
 
-          {/* Là giao dịch gọi đến đối tác */}
-          <Grid item xs={12} sm={3}>
-            <label>Là giao dịch gọi đến đối tác</label>
-          </Grid>
-          <Grid item xs={12} sm={9}>
-            <Checkbox defaultChecked color="success" />
-          </Grid>
-          {/* Mô tả */}
-          <Grid item xs={12} sm={3}>
-            <label>Mô tả</label>
-          </Grid>
-          <Grid item xs={12} sm={9}>
-            <TextField
-              onChange={(e) => handleChange(setDescription, e.target.value)}
-              size="small"
-              className="text-field"
-              id="outlined-multiline-static"
-              label="Mô tả"
-              multiline
-              rows={3}
-              error={Boolean(description === "" ? true : false)}
-              helperText={description === "" ? "Vui lòng không để trống" : ""}
-            />
-          </Grid>
+                    {/* Là giao dịch gọi đến đối tác */}
+                    <Grid item xs={12} sm={3}>
+                      <label>Là giao dịch gọi đến đối tác</label>
+                    </Grid>
+                    <Grid item xs={12} sm={9}>
+                      <Checkbox defaultChecked color="success" />
+                    </Grid>
+                    {/* Mô tả */}
+                    <FastField
+                      name="description"
+                      component={InputField}
+                      label="Mô tả"
+                      placeholder="Mô tả"
+                    />
 
-          {/* Thứ tự hiển thị */}
-          <Grid item xs={12} sm={3}>
-            <label>Thứ tự hiển thị</label>
-          </Grid>
-          <Grid item xs={12} sm={9}>
-            <TextField
-              onChange={(e) => onChangeIdNumber(setNumber, e.target.value)}
-              size="small"
-              className="text-field"
-              autoComplete="off"
-              id="outlined-basic"
-              label="Thứ tự hiển thị"
-              variant="outlined"
-              error={
-                Boolean(number === "" ? true : false) ||
-                validateNumber === false
-              }
-              helperText={
-                number === ""
-                  ? "Vui lòng không để trống"
-                  : "" || validateNumber === false
-                  ? "Vui lòng nhập số"
-                  : ""
-              }
-            />
-          </Grid>
+                    {/* Thứ tự hiển thị */}
+                    <FastField
+                      name="number"
+                      component={InputField}
+                      label="Thứ tự hiển thị"
+                      placeholder="Thứ tự hiển thị"
+                    />
 
-          {/* Kích hoạt */}
-          <Grid item xs={12} sm={3}>
-            <label>Kích hoạt</label>
-          </Grid>
-          <Grid item xs={12} sm={9}>
-            <Checkbox defaultChecked color="success" />
-          </Grid>
+                    {/* Kích hoạt */}
+                    <Grid item xs={12} sm={3}>
+                      <label>Kích hoạt</label>
+                    </Grid>
+                    <Grid item xs={12} sm={9}>
+                      <Checkbox defaultChecked color="success" />
+                    </Grid>
 
-          {/* Hệ thống */}
-          <Grid item xs={12} sm={3}>
-            <label>Hệ thống</label>
-          </Grid>
-          <Grid item xs={12} sm={9}>
-            <Checkbox defaultChecked color="success" />
-          </Grid>
-        </Grid>
-        <Btn>
-          <Button onClick={handleSubmit} variant="contained">
-            Cập nhật
-          </Button>
-        </Btn>
+                    {/* Hệ thống */}
+                    <Grid item xs={12} sm={3}>
+                      <label>Hệ thống</label>
+                    </Grid>
+                    <Grid item xs={12} sm={9}>
+                      <Checkbox defaultChecked color="success" />
+                    </Grid>
+                  </Grid>
+                  <Btn>
+                    <Button type="submit" variant="contained">
+                      Cập nhật
+                    </Button>
+                  </Btn>
+                </Form>
+              </>
+            );
+          }}
+        </Formik>
 
         {/* alert results */}
         <Snackbar
---
+++ src/custom-fields/inputField/index.js
@@ -0,0 +1,48 @@
+import React from "react";
+
+// material-ui
+import TextField from "@mui/material/TextField";
+import { Grid } from "@mui/material";
+
+const InputField = (props) => {
+  const {
+    field,
+    form, // fastfeild
+    type,
+    label,
+    placeholder, // truyền vô
+  } = props;
+
+  const { name } = field;
+  const { errors, touched } = form;
+  const showError = errors[name] && touched[name];
+
+  // name={name}
+  // id={name}
+  // value={value}
+  // onChange={onChange}
+  // onBlur={onBlur}
+
+  return (
+    <>
+      <Grid item xs={12} sm={3}>
+        {label && <label htmlFor={name}>{label}</label>}
+      </Grid>
+      <Grid item xs={12} sm={9}>
+        <TextField
+          {...field}
+          type={type}
+          size="small"
+          className="text-field"
+          autoComplete="off"
+          label={placeholder}
+          variant="outlined"
+          error={Boolean(showError ? true : false)}
+          helperText={showError ? errors[name] : ""}
+        />
+      </Grid>
+    </>
+  );
+};
+
+export default InputField;
